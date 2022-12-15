6cee5c03876fdbb408056e78a500c82eff0a712a
dmounoud <didier.mounoud@proton.me>
2022-12-16 00:33:57 +0100

    test openai

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,13 @@ import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
 
+import { Configuration, OpenAIApi } from 'openai'
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export default function Home() {
   return (
     <div className="container">
@@ -24,3 +31,18 @@ export default function Home() {
     </div>
   )
 }
+
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Create 1 exam questions for my interview with a waiter:\n",
+  temperature: 0,
+  max_tokens: 100,
+  top_p: 1,
+  frequency_penalty: 0.0,
+  presence_penalty: 0.0,
+  stop: ["\n"],
+});
+
+const question = document.getElementById('question');
+
+question.value = response;
